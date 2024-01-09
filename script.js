77e106035b66a5a4297722045b8a632d867d516e
Bruno Teixeira <brunoteixlps@gmail.com>
2024-01-09 19:53:14 -0300

    12° commit - Últimas adições no código JavaScript

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const locations = [
     {
         name: "Kill monster",
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, goTown, easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
@@ -84,6 +84,12 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+    },
+    {
+        name: "easter egg",
+        "button text": ["2", "8", "Go to town square?"],
+        "button functions": [pickTwo, pickEight, goTown],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 
 ];
@@ -204,19 +210,42 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks." 
     text.innerText = " You attack it with your " + weapons[currentWeapon].name + "."
     
-    health -= monsters[fighting].level
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random()  * xp) + 1
-    
+    health -= getMonsterAttackValue(monsters[fighting].level)
+
+    if (isMonsterHit()) {
+        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random()  * xp) + 1  
+    } else {
+        text.innerText += " You miss."
+    }
+
     healthText.innerText = health
     monsterHealthText.innerText = monsterHealth
 
-    if(health <= 0) {
+    if (health <= 0) {
         lose()
     } else if (monsterHealth <= 0) {
         fighting === 2 ? winGame() : defeatMonster()
     }
 
+    if (Math.random() <= .1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks."
+        currentWeapon --
+    }
+
 }
+
+function getMonsterAttackValue(level) {
+
+    const hit = (level * 5) - (Math.floor(Math.random() * xp))
+    console.log(hit)
+    return hit > 0 ? hit : 0
+
+}
+
+function isMonsterHit() {
+    return Math.random() > .2 || health < 20
+}
+
 function dodge() {
 
     text.innerText = "You dodge the attack from the " + monsters[fighting].name + "."
@@ -260,3 +289,39 @@ function restart () {
     goTown()
      
 }
+
+function easterEgg () {
+    update(locations[7])
+}
+
+function pickTwo () {
+    pick(2)
+}
+
+function pickEight () {
+    pick(8)
+}
+
+function pick (guess) {
+    const numbers = []
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11))
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n"
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n"
+    }
+    if (numbers.indexOf(guess) !== -1) {
+        text.innerText += "Right! You win 20 gold!"
+        gold += 20
+        goldText.innerText = gold
+    } else {
+        text.innerText += "Wrong! You lose 10 health!"
+        health -= 10
+        healthText.innerText = health
+        if (health <= 0) {
+            lose()
+        }
+    }
+}
+ 
